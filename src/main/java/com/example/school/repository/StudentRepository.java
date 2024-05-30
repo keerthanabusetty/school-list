00c3ac62b06bcc8ea16f320fa062f1bb55ae1251
keerthanabusetty <busetty14keerthana@gmail.com>
2024-05-30 06:26:14 +0000

    school

---
+++ src/main/java/com/example/school/repository/StudentRepository.java
@@ -0,0 +1,18 @@
+// Write your code here
+package com.example.school.repository;
+
+import com.example.school.model.Student;
+
+import java.util.*;
+
+public interface StudentRepository {
+    ArrayList<Student> getStudents();
+
+    Student getStudentById(int studentId);
+
+    Student addStudent(Student student);
+
+    Student updateStudent(int studentId, Student student);
+
+    void deleteStudent(int studentId);
+}
